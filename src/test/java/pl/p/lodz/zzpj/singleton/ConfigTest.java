012ca59a69c8ea5b000db1b222f1e0b3a61f32f7
jploszaj <71169617+jploszaj@users.noreply.github.com>
2022-03-30 07:52:41 +0200

    all is done
    singleton implementation added

--- src/test/java/pl/p/lodz/zzpj/singleton/ConfigTest.java
+++ src/test/java/pl/p/lodz/zzpj/singleton/ConfigTest.java
@@ -1,4 +1,14 @@
 package pl.p.lodz.zzpj.singleton;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ConfigTest {
+    @Test
+    public void shouldBeInitializedOnce() {
+        Config config1 = Config.getInstance();
+        Config config2 = Config.getInstance();
+        assertEquals(config1,config2);
+    }
 }
